f7ed07443a18c77ce064317f379b523395ffe8df
Lucas Gustavo Hillesheim <lucashillesheim11@gmail.com>
2021-08-18 22:08:04 -0300

    117-Listing categories

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,25 +1,27 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the CategoriasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+import { CategoriaService } from '../../services/domain/categoria.service';
 @IonicPage()
 @Component({
-  selector: 'page-categorias',
-  templateUrl: 'categorias.html',
+    selector: 'page-categorias',
+    templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public categoriaService: CategoriaService) {
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
-  }
+    ionViewDidLoad() {
+        this.categoriaService
+            .findAll()
+            .subscribe(response => {
+                console.log(response);
+            }, error => {
+                console.log(error);
+            })
+    }
 
 }
