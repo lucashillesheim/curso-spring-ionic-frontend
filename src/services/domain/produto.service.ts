057c7ff511d0967f04220686955747d5148cf140
Lucas Gustavo Hillesheim <lucashillesheim11@gmail.com>
2021-08-24 22:27:44 -0300

    138-Loading product's image

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Rx";
 import { API_CONFIG } from "../../config/api.config";
 import { ProdutoDTO } from "../../models/produto.dto";
 
@@ -11,5 +12,10 @@ export class ProdutoService {
 
     findByCategoria(categoriaId: string) {
         return this.http.get<Array<ProdutoDTO>>(`${API_CONFIG.baseUrl}/products/?categorias=${categoriaId}`);
+    }
+
+    getThumbnailFromBucket(id: string): Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+        return this.http.get(url, { responseType: 'blob' });
     }
 }
