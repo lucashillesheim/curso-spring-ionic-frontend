0e8d8ccaade58896093068698c346747575716e4
Lucas Gustavo Hillesheim <lucashillesheim11@gmail.com>
2021-08-26 23:24:43 -0300

    142-Concluding basket functionality

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { API_CONFIG } from '../../config/api.config';
 import { CartItem } from '../../models/cart-item';
+import { ProdutoDTO } from '../../models/produto.dto';
 import { CartService } from '../../services/domain/cart.service';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -36,6 +37,26 @@ export class CartPage {
                 },
                     error => { })
         }
+    }
+
+    removeItem(produto: ProdutoDTO) {
+        this.itens = this.cartService.removeProduto(produto).itens
+    }
+
+    increaseQuantity(produto: ProdutoDTO) {
+        this.itens = this.cartService.increaseQuantity(produto).itens
+    }
+
+    decreaseQuantity(produto: ProdutoDTO) {
+        this.itens = this.cartService.decreaseQuantity(produto).itens
+    }
+
+    total(): number {
+        return this.cartService.total()
+    }
+
+    goOn() {
+        this.navCtrl.setRoot('CategoriasPage')
     }
 
 }
--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Cart } from "../../models/cart";
+import { CartItem } from "../../models/cart-item";
 import { ProdutoDTO } from "../../models/produto.dto";
 import { StorageService } from "../storage.service";
 
@@ -20,13 +21,47 @@ export class CartService {
     }
 
     addProduto(produto: ProdutoDTO): Cart {
+        return this.handleItem(produto, (_, position, cart) => {
+            position === -1 && cart.itens.push({ produto, quantidade: 1 });
+        });
+    }
+
+    removeProduto(produto: ProdutoDTO): Cart {
+        return this.handleItem(produto, (_, position, cart) => {
+            position != -1 && this.removeItem(position, cart);
+        });
+    }
+
+    removeItem(position: number, cart: Cart) {
+        cart.itens.splice(position, 1);
+    }
+
+    increaseQuantity(produto: ProdutoDTO): Cart {
+        return this.handleItem(produto, (cartItem, position) => {
+            position != -1 && cartItem.quantidade++;
+        });
+    }
+
+    decreaseQuantity(produto: ProdutoDTO): Cart {
+        return this.handleItem(produto, (cartItem, position, cart) => {
+            position != -1 && cartItem.quantidade--;
+            if (cartItem.quantidade <= 0)
+                this.removeItem(position, cart);
+        });
+    }
+
+    handleItem(produto: ProdutoDTO, predicate: (cartItem: CartItem, position: number, cart: Cart) => void): Cart {
         let cart: Cart = this.getCart();
         let position = cart.itens.findIndex(item => item.produto.id === produto.id);
-        if (position === -1) {
-            cart.itens.push({ produto: produto, quantidade: 1 });
-        }
+        predicate(cart.itens[position], position, cart);
         this.storage.setCart(cart);
         return cart;
+    }
+
+    total(): number {
+        return this.getCart().itens
+            .map(item => item.produto.preco * item.quantidade)
+            .reduce((prev, value) => prev + value, 0);
     }
 
 }
