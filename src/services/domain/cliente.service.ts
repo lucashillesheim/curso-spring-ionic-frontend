385ffa8edb7933482b1f4e462236498427e8987f
Lucas Gustavo Hillesheim <lucashillesheim11@gmail.com>
2021-08-28 17:18:16 -0300

    150-Order confirmation page

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -11,6 +11,10 @@ export class ClienteService {
     constructor(public http: HttpClient, public storage: StorageService) {
     }
 
+    findById(id: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clients/${id}`)
+    }
+
     findByEmail(email: string) {
         return this.http.get(`${API_CONFIG.baseUrl}/clients/email?value=${email}`)
     }
