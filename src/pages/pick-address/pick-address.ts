6efcdf3e5b1f5b630e50de255241525d001bf7ea
Lucas Gustavo Hillesheim <lucashillesheim11@gmail.com>
2021-08-28 16:41:53 -0300

    148-Getting order's data

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { StorageService } from '../../services/storage.service';
 
@@ -12,12 +14,14 @@ import { StorageService } from '../../services/storage.service';
 export class PickAddressPage {
 
     items: Array<EnderecoDTO>
+    pedido: PedidoDTO
 
     constructor(
         public navCtrl: NavController,
         public navParams: NavParams,
         public storage: StorageService,
-        public clienteService: ClienteService,) {
+        public clienteService: ClienteService,
+        public cartService: CartService) {
     }
 
     ionViewDidLoad() {
@@ -26,6 +30,21 @@ export class PickAddressPage {
             this.clienteService.findByEmail(localUser.email)
                 .subscribe(response => {
                     this.items = response['enderecos']
+
+                    let cart = this.cartService.getCart()
+
+                    this.pedido = {
+                        cliente: { id: response['id'] },
+                        enderecoDeEntrega: null,
+                        pagamento: null,
+                        items: cart.itens
+                            .map(item => {
+                                return {
+                                    quantidade: item.quantidade,
+                                    produto: { id: item.produto.id }
+                                }
+                            })
+                    }
                 },
                     error => {
                         if (error.status == 403) {
@@ -35,6 +54,12 @@ export class PickAddressPage {
         } else {
             this.navCtrl.setRoot('HomePage')
         }
+
+    }
+
+    nextPage(endereco: EnderecoDTO) {
+        this.pedido.enderecoDeEntrega = { id: endereco.id }
+        console.log(this.pedido);
 
     }
 
