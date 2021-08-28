ce0eecc1cfddbb48b2632d949c0f69111843f101
Lucas Gustavo Hillesheim <lucashillesheim11@gmail.com>
2021-08-28 15:58:20 -0300

    143-Access to basket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,7 @@ export class MyApp {
         this.pages = [
             { title: 'Profile', component: 'ProfilePage' },
             { title: 'Categories', component: 'CategoriasPage' },
+            { title: 'Basket', component: 'CartPage' },
             { title: 'Logout', component: '' }
         ];
 
